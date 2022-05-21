53f7c434babe81df5ad8d685db7db733ea9a32d7
leiwingqueen <leiwingqueen@gmail.com>
2022-05-21 17:32:51 +0800

    最少移动次数使数组元素相等

--- math/MinMoves2.go
+++ math/MinMoves2.go
@@ -1,5 +1,7 @@
 package math
 
+import "sort"
+
 //给你一个长度为 n 的整数数组 nums ，返回使所有数组元素相等需要的最少移动数。
 //
 //在一步操作中，你可以使数组中的一个元素加 1 或者减 1 。
@@ -31,6 +33,7 @@ package math
 //著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
 //假设所有数字都是正数是不是会变得很简单
+//错误
 func minMoves2(nums []int) int {
 	sum := int64(0)
 	min := 1_000_000_001
@@ -44,4 +47,20 @@ func minMoves2(nums []int) int {
 		sum += int64(len(nums) * min)
 	}
 	return int(sum / int64(len(nums)))
+}
+
+//有一个结论，选择中位数其实就是最优解
+func minMoves3(nums []int) int {
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i] < nums[j]
+	})
+	l := 0
+	r := len(nums) - 1
+	res := 0
+	for l < r {
+		res += nums[r] - nums[l]
+		l++
+		r--
+	}
+	return res
 }
